495771532416d3a1fe1aef8a629cd84d75b3a3c3
Gabe Luethje <gabe.luethje@ifit.com>
2022-05-26 11:15:56 -0700

    Add Trivia API request methods

---
+++ src/api/index.ts
@@ -0,0 +1,36 @@
+const API_URL = 'https://opentdb.com';
+type Options = {
+  amount: number;
+  category?: number;
+  difficulty?: 'easy' | 'medium' | 'hard' | 'mixed';
+};
+export const getQuestions = async ({
+  amount = 10,
+  category = -1,
+  difficulty = 'mixed'
+}) => {
+  difficulty = difficulty.toLowerCase();
+  const url =
+    `${API_URL}/api.php?amount=${amount}` +
+    (category !== -1 ? `&category=${category}` : '') +
+    (difficulty !== 'mixed' ? `&difficulty=${difficulty}` : '');
+
+  const response = await fetch(url);
+
+  if (response.status !== 200) {
+    throw new Error('There was a problem fetching the questions');
+  }
+
+  const data = await response.json();
+  return data.results;
+};
+
+export const getCategories = async () => {
+  const response = await fetch(`${API_URL}/api_category.php`);
+  if (response.status !== 200) {
+    throw new Error('There was a problem fetching the categories');
+  }
+
+  const data = await response.json();
+  return data.trivia_categories;
+};
